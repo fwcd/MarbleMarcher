e151f867e84a03523451ec7cf656db66e9d0b9cc
fwcd <fwcdmail@gmail.com>
2019-01-01 16:38:02 +0100

    Changed OpenGL version to 2

--- assets/vert.glsl
+++ assets/vert.glsl
@@ -16,6 +16,7 @@
 */
 #version 120
 #extension GL_EXT_gpu_shader4 : require
+
 const vec2 quadVertices[4] = vec2[4]( vec2(-1.0, -1.0), vec2(1.0, -1.0), vec2(-1.0, 1.0), vec2(1.0, 1.0) );
 void main() {
 	gl_Position = vec4(quadVertices[gl_VertexID], 0.0, 1.0);
